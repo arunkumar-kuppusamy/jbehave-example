06cd5d84ccf8f37276344784db0c001787f24f76
Sheldon Hearn <sheldonh@starjuice.net>
2014-12-11 16:25:15 +0200

    Clarify lack of domain component injection in imperative test provider

--- jbehave-example-api/src/test/java/net/starjuice/calculator/acceptance_tests/api/ApiTestProvider.java
+++ jbehave-example-api/src/test/java/net/starjuice/calculator/acceptance_tests/api/ApiTestProvider.java
@@ -9,11 +9,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApiTestProvider implements ImperativeTestProvider {
 
-/*
-    // Trying this gave: org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'apiImperativeTestProvider': Injection of autowired dependencies failed; nested exception is org.springframework.beans.factory.BeanCreationException: Could not autowire field: private net.starjuice.calculator.api.Calculator net.starjuice.calculator.acceptance_tests.api.ApiImperativeTestProvider.calculator; nested exception is org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type [net.starjuice.calculator.api.Calculator] found for dependency: expected at least 1 bean which qualifies as autowire candidate for this dependency. Dependency annotations: {@org.springframework.beans.factory.annotation.Autowired(required=true)}
-    @Autowired
-*/
+    // TODO ApiTestProvider needs a @Configuration that sets @ComponentScan basePackages for @Autowired to work
+    //@Autowired
     private Calculator calculator;
+
     private int actual;
 
     @Override
